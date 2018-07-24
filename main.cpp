474bfb296905d4c1fa7bce082ea44b9c9c2d040f
Anurag Sahoo <anurag@aitoelabs.com>
2018-07-24 16:51:18 -0700

    feat(math/linalg): Linalg module with basic vector and matrix operations

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "math/linalg/vector.hpp"
+#include "math/linalg/matrix.hpp"
+
+int main() {
+    try {
+
+
+    } catch (const char* exception) {
+        std::cout << exception << std::endl;
+    }
+}
